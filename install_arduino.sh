e4029ce882e1008093dd3b041fca90a0b5698340
SergioPoverony <sergiopoverony@gmail.com>
2020-03-29 14:08:28 +0500

    Dance again )) not read commit description )

--- install_arduino.sh
+++ install_arduino.sh
@@ -10,13 +10,15 @@ echo "Configuring Battery to start at boot..."
 
 #boot script should have the necessary lines(Battery, poweroff, backlight, buttons), then new ones should be added after a y/n question(joystick)
 # Insert battery script into rc.local before final 'exit 0'
-if ! grep -Fxq "python /home/pi/battery_status/battery_arduino.py &" /etc/rc.local
-then
-sed -i "s/^exit 0/python \/home\/pi\/battery_status\/battery_arduino.py \&\\nexit 0/g" /etc/rc.local >/dev/null
-fi
-
-
+#if ! grep -Fxq "python /home/pi/battery_status/battery_arduino.py &" /etc/rc.local
+#then
+#sed -i "s/^exit 0/python \/home\/pi\/battery_status\/battery_arduino.py \&\\nexit 0/g" /etc/rc.local >/dev/null
+#fi
 chmod 0775 /home/pi/battery_status/pngview
+sudo cp /home/pi/battery_status/battery.service /etc/systemd/system/battery.service
+sudo chmod +x /etc/systemd/system/battery.service
 read -rsp $'Press any key to reboot...\n' -n1 key
 reboot
+
+
 
